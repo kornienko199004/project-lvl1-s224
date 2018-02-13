22760f60c7c4671cda1e17e0b9e66702ee58c948
kornienko199004 <kornienko199004@gmail.com>
2018-02-13 14:17:55 +0300

    Add eslint

--- Makefile
+++ Makefile
@@ -2,4 +2,10 @@ install:
 	npm install
 
 start:
-	npm run babel-node -- src/bin/brain-games.js 
+	npm run babel-node -- src/bin/brain-games.js
+
+pubish:
+	npm publish
+
+lint:
+	npm run eslint -- src
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-    const userName = readlineSync.question('May I have you name? ');
-    console.log(`Hello, ${userName}!`);
+  const userName = readlineSync.question('May I have you name? ');
+  console.log(`Hello, ${userName}!`);
 };
+
