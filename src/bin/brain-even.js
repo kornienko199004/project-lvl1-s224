ef1e0610f33df717a854017473f9c587c875a97a
kornienko199004 <kornienko199004@gmail.com>
2018-02-14 12:03:09 +0300

    brain-even, brain-games -> change with askQuestionLoop

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+import showWelcomeMessage from '../index';
 import even from '../games/even';
+import askQuestionLoop from '../modules/askQuestionLoop';
 
-even();
+const userName = showWelcomeMessage('Answer "yes" if number even otherwise answer "no"');
+const numberOfQuestions = 3;
+
+askQuestionLoop(even, userName, numberOfQuestions);
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import getUserName from '../index';
+import showWelcomeMessage from '../index';
 
-console.log('Welcome to the Brain Games!');
-getUserName();
+showWelcomeMessage();
